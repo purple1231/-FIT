ffb2e605050d201317e68b75607c0d4dded36803
purple1231 <87258160+purple1231@users.noreply.github.com>
2025-12-26 20:10:39 +0900

    fix(db): Clear database connection credentials
    Updated database connection details to be empty for configuration.

--- db/db.js
+++ db/db.js
@@ -2,9 +2,9 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'sql.freedb.tech',
-  user: 'freedb_royaa',       // 사용자에 맞게 변경
-  password: '?rJ5bdVeR@sAP?j',   // 비밀번호에 맞게 변경
+  host: '',
+  user: '',       // 사용자에 맞게 변경
+  password: '',   // 비밀번호에 맞게 변경
   database: 'freedb_FitDataBase',
   waitForConnections: true,
   connectionLimit: 10,
